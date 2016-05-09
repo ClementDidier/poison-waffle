444bfd874621348dc85a8df3b0d50bf4995c6f58
andrev <andrev@127.0.0.1>
2016-05-09 17:03:11 +0200

    Ajout du menu dans la fenetre de jeu et de la classe buttonMenuListener

--- poisonned-waffle-groupe1/src/Window.java
+++ poisonned-waffle-groupe1/src/Window.java
@@ -1,16 +1,48 @@
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class Window implements Runnable
 {
 	private static final String DEFAULT_NAME = "Poisonned Waffle";
 	private JFrame frame;
 	private GraphicsPanel panel;
+	private JMenuBar menuBar;
 	
 	public Window(int width, int height)
 	{
 		this.frame = new JFrame();
 		this.frame.setName(DEFAULT_NAME);
 		this.frame.setSize(width, height);
+		this.menuBar = new JMenuBar();
+		
+		JMenu redoUndo = new JMenu("Historique");
+		JMenu loadSave = new JMenu("Partie");
+		
+		JMenuItem redo = new JMenuItem("Refaire");
+		JMenuItem undo = new JMenuItem("Annuler");
+		
+		redoUndo.add(redo);
+		redoUndo.add(undo);
+		
+		JMenuItem save = new JMenuItem("Sauvegarder");
+		JMenuItem load = new JMenuItem("Charger");
+		JMenuItem newGame = new JMenuItem("Nouvelle Partie");
+		
+		loadSave.add(newGame);
+		loadSave.add(load);
+		loadSave.add(save);
+		
+		this.menuBar.add(redoUndo);
+		this.menuBar.add(loadSave);
+		
+		this.frame.setJMenuBar(this.menuBar);
+		
+		
 		
 		this.panel = new GraphicsPanel();
 		
