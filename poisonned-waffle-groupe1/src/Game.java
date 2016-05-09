af305ceece79bf0ee49a8f581a3716646fc63358
ClementDidier <c.didier.contact@gmail.com>
2016-05-09 17:01:42 +0200

    Début implémentation des parties (Game)

--- poisonned-waffle-groupe1/src/Game.java
+++ poisonned-waffle-groupe1/src/Game.java
@@ -1,10 +1,48 @@
 
 public class Game implements GameInterface{
-
+	
+	private static final int DEFAULT_WIDTH = 6;
+	private static final int DEFAULT_HEIGHT = 4;
+	
+	/**
+	 * Tableau de case (gaufre)
+	 */
+	protected BoardInterface board;
+	/**
+	 * Joueur 1 : joue le premier tour
+	 */
+	protected PlayerInterface player1;
+	/**
+	 * Joueur 2 : joue en second
+	 */
+	protected PlayerInterface player2;
+	/**
+	 * Joueur courant
+	 */
+	protected PlayerInterface currentPlayer;
+	
+	
+	public Game(PlayerInterface p1, PlayerInterface p2)
+	{
+		this.board = new Board(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	}
+	
+	public Game(BoardInterface board, PlayerInterface p1, PlayerInterface p2)
+	{
+		this.board = board;
+	}
+	
+	/**
+	 * Determine si la partie est terminée
+	 */
 	@Override
 	public boolean isTerminated() {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			return this.board.getCell(0, 1) == Cell.EATEN && this.board.getCell(1, 0) == Cell.EATEN;
+		} catch (OutOfWaffleException e) {
+			System.err.println(e.getMessage());
+			return true;
+		}
 	}
 
 	@Override
