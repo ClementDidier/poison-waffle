ba696093c47dc375aa2d1db9babf8b26bbbce402
ClementDidier <c.didier.contact@gmail.com>
2016-05-10 12:49:02 +0200

    Attente active de l'utilisateur PlayerMouse (click souris)

--- poisonned-waffle-groupe1/src/entities/PlayerMouse.java
+++ poisonned-waffle-groupe1/src/entities/PlayerMouse.java
@@ -4,15 +4,35 @@ import utilities.Vector2;
 
 public class PlayerMouse extends Player {
 
+	private boolean clickEventRaised;
+	private Vector2 clickLocation;
+	
 	public PlayerMouse(String name) {
 		super(name);
-		// TODO Auto-generated constructor stub
+		this.clickEventRaised = false;
+		this.clickLocation = new Vector2();
 	}
 
 	@Override
 	public Vector2 play() {
-		// TODO Auto-generated method stub
-		return null;
+		// Attente de l'utilisateur
+		while(!this.clickEventRaised)
+		{
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				System.err.println(e.getMessage());
+				e.printStackTrace();
+			}
+		}
+		this.clickEventRaised = false;
+		
+		return this.clickLocation;
 	}
-
+	
+	public void setClickEvent(Vector2 clickLocation)
+	{
+		this.clickEventRaised = true;
+		this.clickLocation = clickLocation;
+	}
 }
