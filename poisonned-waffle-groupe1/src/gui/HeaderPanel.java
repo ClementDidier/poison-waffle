c3346789d41e9974eb00a874d639ac0a063df481
Lucas <Lucas@130.190.29.54>
2016-05-10 13:25:22 +0200

    Version clean HeaderPanel

--- poisonned-waffle-groupe1/src/gui/HeaderPanel.java
+++ poisonned-waffle-groupe1/src/gui/HeaderPanel.java
@@ -1,8 +1,10 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
 import java.awt.image.BufferedImage;
 
 import javax.swing.ImageIcon;
@@ -20,42 +22,12 @@ public class HeaderPanel extends JPanel {
 	    private GridBagConstraints gbc;
 
 	    public HeaderPanel() {
-	        r = new Random();
-	        setOpaque(true);
-	        setBackground(getRandomColor());
-	        setLayout(new BorderLayout(GAP, GAP));      
-	        JPanel footerPanel = getPanel();
-	        footerPanel.setLayout(new GridBagLayout());
-	        gbc = new GridBagConstraints();
-	        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-	        gbc.insets = new Insets(GAP, GAP, GAP, GAP);
-	        gbc.gridx = 0;
-	        gbc.gridy = 0;
-	        gbc.weightx = 0.8;
-	        gbc.weighty = 1.0;
-	        leftPanel = getPanel();
-	        footerPanel.add(leftPanel, gbc);
-	        gbc.gridx = 1;
-	        gbc.weightx = 0.2;
-	        JLabel rightLabel = new JLabel("Right Label", JLabel.CENTER);
-	        footerPanel.add(rightLabel);
 
-	        JLabel centerLabel = new JLabel("Main Panel", JLabel.CENTER);
-	        add(centerLabel, BorderLayout.CENTER);
-	        add(footerPanel, BorderLayout.PAGE_END);
 	    }
 
 	    private JPanel getPanel() {
 	        JPanel panel = new JPanel();
-	        panel.setOpaque(true);
-	        panel.setBackground(getRandomColor());
-
 	        return panel;
-	    }
-
-	    private Color getRandomColor() {
-	        return new Color(r.nextFloat(), r.nextFloat(),
-	                            r.nextFloat(), r.nextFloat());
 	    }
 
 	    @Override
