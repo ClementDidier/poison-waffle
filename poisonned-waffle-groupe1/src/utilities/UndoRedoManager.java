5fc7162fb5d72588a5467ea8d5e19429361ef3dc
ClementDidier <c.didier.contact@gmail.com>
2016-05-10 10:13:56 +0200

    Implémentation UndoRedoManager

--- poisonned-waffle-groupe1/src/utilities/UndoRedoManager.java
+++ poisonned-waffle-groupe1/src/utilities/UndoRedoManager.java
@@ -1,4 +1,5 @@
 package utilities;
+import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class UndoRedoManager<T> 
@@ -6,19 +7,38 @@ public class UndoRedoManager<T>
 	Stack<T> undoStack;
 	Stack<T> redoStack;
 	
-	public void undo()
+	public boolean canUndo()
 	{
-		// TODO: a remplir
+		return !this.undoStack.empty();
 	}
 	
-	public void redo()
+	public boolean canRedo()
 	{
-		// TODO: a remplir
+		return !this.redoStack.empty();
 	}
 	
-	public void add()
+	public void undo() throws EmptyStackException
 	{
-		// TODO: a remplir
+		if(!this.canUndo())
+			throw new EmptyStackException();
+		
+		T obj = this.undoStack.pop();
+		this.redoStack.push(obj);
+	}
+	
+	public void redo() throws EmptyStackException
+	{
+		if(!this.canRedo())
+			throw new EmptyStackException();
+		
+		T obj = this.redoStack.pop();
+		this.undoStack.push(obj);
+	}
+	
+	public void add(T obj)
+	{
+		this.undoStack.push(obj);
+		this.redoStack.clear();
 	}
 	
 	
