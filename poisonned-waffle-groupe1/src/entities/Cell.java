264166955ea3e664926aec6f49619451dd8327a3
andrev <andrev@127.0.0.1>
2016-05-10 13:18:40 +0200

    Implémentation de GraphicsPanel

--- poisonned-waffle-groupe1/src/entities/Cell.java
+++ poisonned-waffle-groupe1/src/entities/Cell.java
@@ -1,7 +1,37 @@
 package entities;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public enum Cell{
-	EATEN,
-	POISONNED(),
-	CLEAN
+
+	CLEAN("../resources/CaseSaine.png"),
+	POISONNED("../resources/CaseEmpoisonee.png"),
+	EATEN("");
+	
+	private String path;
+	private BufferedImage apparence;
+	
+	private Cell(String path){
+		this.path = path;
+		this.apparence = null;
+		try{
+			this.apparence = ImageIO.read(new File(this.path));
+		}catch(IOException e){
+			System.err.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public String getPath(){
+		return this.path;
+	}
+	
+	public BufferedImage getApparence(){
+		return this.apparence;
+	}
+	
 }
--- poisonned-waffle-groupe1/src/gui/GraphicsPanel.java
+++ poisonned-waffle-groupe1/src/gui/GraphicsPanel.java
@@ -5,21 +5,51 @@ import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
+import javax.swing.ImageIcon;
 import javax.swing.JComponent;
+
+import entities.Cell;
+import exceptions.OutOfWaffleException;
+import interfaces.GameInterface;
+import utilities.Vector2;
 
 public class GraphicsPanel extends JComponent
 {
 	private static final long serialVersionUID = 1L;
+	private GameInterface game;
+	private Vector2 offset;
+	
+	public GraphicsPanel(GameInterface game) {
+		this.game = game;
+		this.offset = new Vector2();
+	}
 
 	@Override
 	protected void paintComponent(Graphics g)
 	{
 		super.paintComponent(g);
 		Graphics2D batch = (Graphics2D) g;
-		BufferedImage cell =
-		
 		this.clear(batch);
+		BufferedImage cell = new BufferedImage(this.getWidth(), this.getHeight(), BufferedImage.TYPE_INT_ARGB);
 		
+		for(int x = 0; x<this.game.getBoard().getSize().getWidth(); x++)
+		{
+			for(int y = 0; y<this.game.getBoard().getSize().getHeight(); y++){
+				Cell currentCell;
+				try {
+					currentCell = this.game.getBoard().getCell(x, y);
+				
+					if(!currentCell.equals(Cell.EATEN)) {
+						BufferedImage currentApparence = currentCell.getApparence();
+						batch.drawImage(currentApparence, this.offset.getX(), this.offset.getY(), currentApparence.getWidth(), currentApparence.getHeight(), null);
+					}
+				} catch (OutOfWaffleException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+					
+			}
+		}
 		/* Something to draw */
 	}
 	
