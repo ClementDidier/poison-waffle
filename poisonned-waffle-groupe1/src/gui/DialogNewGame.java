6ebe6bbeca3a62df4d05a2e28d2606e5ecdd18ef
andrev <andrev@127.0.0.1>
2016-05-11 11:14:49 +0200

    Implémentation d'une boite de dialogue perso pour une nouvelle partie

--- poisonned-waffle-groupe1/src/gui/DialogNewGame.java
+++ poisonned-waffle-groupe1/src/gui/DialogNewGame.java
@@ -1,7 +1,10 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Set;
 
 import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
@@ -9,6 +12,8 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import interfaces.PlayerInterface;
 
@@ -21,7 +26,7 @@ public class DialogNewGame extends JDialog{
 	    super(parent, title, modal);
 	    
 	    this.window = window;
-	    this.setSize(200, 80);
+	    this.setSize(600, 300);
 	    this.setLocationRelativeTo(null);
 	    this.setResizable(false);
 	    this.initComponent();
@@ -32,17 +37,51 @@ public class DialogNewGame extends JDialog{
 	private void initComponent(){
 		JPanel content = new JPanel();
 		
+		//===========Joueur 1 =========//
 		JPanel panPlayer1 = new JPanel();
+		panPlayer1.setLayout(new GridLayout(2, 2));
 		
 		panPlayer1.setBorder(BorderFactory.createTitledBorder("Joueur 1"));
+		
 		JLabel labelTypePlayer1 = new JLabel("Type du joueur1");
 		
 		Hashtable<String, Class> listTypePlayer = this.window.getListTypePlayer();
+		Set<String> listLabelTypePlayer = listTypePlayer.keySet();
+		
+		JComboBox<String> comboBoxPlayer1 = new JComboBox<>();
+		for(String s:listLabelTypePlayer){
+			comboBoxPlayer1.addItem(s);
+		}
+		
+		JLabel nameTypePlayer1 = new JLabel("Nom du joueur 1");
+		JTextField textFieldPlayer1 = new JTextField();
 		
 		panPlayer1.add(labelTypePlayer1);
+		panPlayer1.add(comboBoxPlayer1);
+		panPlayer1.add(nameTypePlayer1);
+		panPlayer1.add(textFieldPlayer1);
+		
+		//========= Joueur 2 =========//
+		JPanel panPlayer2 = new JPanel();
+		panPlayer2.setLayout(new GridLayout(2, 2));
+		panPlayer2.setBorder(BorderFactory.createTitledBorder("Joueur 2"));
+		JLabel labelTypePlayer2 = new JLabel("Type du joueur2");
+		
+		JComboBox<String> comboBoxPlayer2 = new JComboBox<>();
+		for(String s:listLabelTypePlayer){
+			comboBoxPlayer2.addItem(s);
+		}
+		
+		JLabel nameTypePlayer2 = new JLabel("Nom du joueur 2");
+		JTextField textFieldPlayer2 = new JTextField();
+		
+		panPlayer2.add(labelTypePlayer2);
+		panPlayer2.add(comboBoxPlayer2);
+		panPlayer2.add(nameTypePlayer2);
+		panPlayer2.add(textFieldPlayer2);
 		
 		content.add(panPlayer1);
-		
+		content.add(panPlayer2);
 		this.getContentPane().add(content, BorderLayout.WEST);
 	}
 }
